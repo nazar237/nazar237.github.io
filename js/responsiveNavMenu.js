5ff84f11180b3c62446cca01f1a2f2b82b989c34
nazar237 <nazar237@gmail.com>
2017-10-25 00:38:16 +0300

    added new js

---
+++ js/responsiveNavMenu.js
@@ -0,0 +1,11 @@
+function navMenu(){
+    var elemOpen = document.getElementById("nav_menu_open");
+    var elemClose = document.getElementById("nav_menu_close");
+
+    if(elemOpen.style.display == "none")
+        elemOpen.style.display == "block", 
+        elemClose.style.display == "none";
+    else
+        elemOpen.style.display == "none",
+        elemClose.style.display == "block";
+};
