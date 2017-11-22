24f43a24f093a6cc20622ff912cbe432708a4ab7
nazar237 <30602125+nazar237@users.noreply.github.com>
2017-11-22 17:28:05 +0200

    added error 404 page, changed mobile menu button in pure css  and some fix

--- js/responsiveNavMenu.js
+++ js/responsiveNavMenu.js
@@ -1,13 +1,5 @@
-function navMenu(){
-	
-	var elemOpen = document.getElementById("nav_menu_open");
-	var elemClose = document.getElementById("nav_menu_close");
+navIcon = document.getElementById("nav__icon");
 
-	if(elemOpen.style.display == "none") {
-		elemOpen.style.display = "inline-block",
-		elemClose.style.display = "none";
-	} else {
-		elemOpen.style.display = "none",
-		elemClose.style.display = "inline-block";
-	}
-};
+	navIcon.addEventListener("click", function() {
+	navIcon.classList.toggle("open");
+});
